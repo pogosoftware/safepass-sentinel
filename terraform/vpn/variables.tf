0237804508f2f00a2cfe1479111a5d5d9b232f28
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-01-15 11:16:59 +0100

    Added missing provider configuration for hcp in vpn

--- terraform/vpn/variables.tf
+++ terraform/vpn/variables.tf
@@ -5,4 +5,10 @@ variable "hcp_orgranization_id" {
 
 variable "hcp_network_workspace_name" {
   description = "The name of network TFC workspace"
+  type        = string
+}
+
+variable "hcp_project_id" {
+  description = "The UUID of HCP project"
+  type        = string
 }
