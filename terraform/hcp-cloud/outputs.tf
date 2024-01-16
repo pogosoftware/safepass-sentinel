294f579186ed3c6613a9ffe21497fe67df7bd7a6
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-01-16 18:43:18 +0100

    Updated boundary_cluster_id output

--- terraform/hcp-cloud/outputs.tf
+++ terraform/hcp-cloud/outputs.tf
@@ -1,6 +1,6 @@
-output "boundary_id" {
-  description = "The ID of HCP Boudary"
-  value       = hcp_boundary_cluster.this.id
+output "boundary_cluster_id" {
+  description = "The ID of HCP Boudary cluster"
+  value       = regex("https://([a-f0-9-]+)\\.boundary\\.hashicorp\\.cloud", hcp_boundary_cluster.this.cluster_url) 
 }
 
 output "boundary_cluster_url" {
