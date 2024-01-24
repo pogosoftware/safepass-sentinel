0917ab1db9123665bd6e6e91c590f912492abd97
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-01-24 20:10:34 +0100

    Added boundary resources

---
+++ terraform/boundary/main.tf
@@ -0,0 +1,38 @@
+####################################################################################################
+### HCP BOUNDARY RESOURCES
+####################################################################################################
+resource "boundary_scope" "org" {
+  name                     = var.hcp_boundary_org_name
+  description              = var.hcp_boundary_org_description
+  scope_id                 = "global"
+  auto_create_admin_role   = true
+  auto_create_default_role = true
+}
+
+resource "boundary_scope" "project" {
+  name        = var.hcp_boundary_project_name
+  description = var.hcp_boundary_project_description
+
+  scope_id                 = boundary_scope.org.id
+  auto_create_admin_role   = true
+  auto_create_default_role = true
+}
+
+resource "boundary_credential_store_vault" "certificates_store" {
+  name     = "certificates-store"
+  address  = local.vault_public_endpoint_url
+  token    = local.vault_client_token
+  scope_id = boundary_scope.project.id
+}
+
+resource "boundary_credential_library_vault_ssh_certificate" "certificates_library" {
+  name                = "certificates-library"
+  credential_store_id = boundary_credential_store_vault.certificates_store.id
+  path                = "ssh-client-signer/sign/boundary-client"
+  username            = "ubuntu"
+  key_type            = "ecdsa"
+  key_bits            = 384
+  extensions = {
+    permit-pty = ""
+  }
+}
---
+++ terraform/boundary/terraform.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.6.6"
+
+  required_providers {
+    hcp = {
+      source  = "hashicorp/hcp"
+      version = "0.79.0"
+    }
+    boundary = {
+      source  = "hashicorp/boundary"
+      version = "1.1.12"
+    }
+  }
+}
