2f2169a0b3827ab0459ec11756aa4ca8ea293aef
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-02-14 13:58:45 +0100

    Refactoring hcp-cloud module

--- terraform/hcp-cloud/locals.tf
+++ terraform/hcp-cloud/locals.tf
@@ -1,4 +1,11 @@
 locals {
+  hcp_network_workspace_name = format("%s-%s", var.hcp_network_workspace_name, var.environment)
+  hcp_vault_variable_set_workspaces = { for workspace in var.hcp_vault_variable_set_workspaces :
+    workspace => format("%s-%s", workspace, var.environment)
+  }
+  hcp_cloud_vault_cluster_id    = format("%s-%s", var.hcp_cloud_vault_cluster_id, var.environment)
+  hcp_cloud_boundary_cluster_id = format("%s-%s", var.hcp_cloud_boundary_cluster_id, var.environment)
+
   # boundary
   boundary_username = random_string.boundary_username.result
   boundary_password = random_password.boundary_password.result
@@ -8,4 +15,6 @@ locals {
   peer_account_id       = data.terraform_remote_state.network.outputs.vpc_owner_id
   peer_vpc_region       = var.aws_region
   peer_destination_cidr = data.terraform_remote_state.network.outputs.vpc_cidr_block
+  peering_id            = format("%s-peering-%s", var.peering_id, var.environment)
+  route_id              = format("%s-hvn-%s", var.route_id, var.environment)
 }
--- terraform/hcp-cloud/main.tf
+++ terraform/hcp-cloud/main.tf
@@ -9,7 +9,7 @@ resource "hcp_hvn" "this" {
 }
 
 resource "hcp_vault_cluster" "this" {
-  cluster_id      = var.hcp_cloud_vault_cluster_id
+  cluster_id      = local.hcp_cloud_vault_cluster_id
   hvn_id          = hcp_hvn.this.hvn_id
   public_endpoint = var.hcp_cloud_vault_public_endpoint
   tier            = var.hcp_cloud_vault_tier
@@ -35,7 +35,7 @@ resource "random_password" "boundary_password" {
 }
 
 resource "hcp_boundary_cluster" "this" {
-  cluster_id = var.hcp_cloud_boundary_cluster_id
+  cluster_id = local.hcp_cloud_boundary_cluster_id
   username   = local.boundary_username
   password   = local.boundary_password
   tier       = var.hcp_cloud_boundary_tier
@@ -46,7 +46,7 @@ resource "hcp_boundary_cluster" "this" {
 ####################################################################################################
 resource "hcp_aws_network_peering" "peer" {
   hvn_id          = hcp_hvn.this.hvn_id
-  peering_id      = var.peering_id
+  peering_id      = local.peering_id
   peer_vpc_id     = local.peer_vpc_id
   peer_account_id = local.peer_account_id
   peer_vpc_region = local.peer_vpc_region
@@ -54,7 +54,7 @@ resource "hcp_aws_network_peering" "peer" {
 
 resource "hcp_hvn_route" "peer_route" {
   hvn_link         = hcp_hvn.this.self_link
-  hvn_route_id     = var.route_id
+  hvn_route_id     = local.route_id
   destination_cidr = local.peer_destination_cidr
   target_link      = hcp_aws_network_peering.peer.self_link
 }
@@ -63,69 +63,45 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
   auto_accept               = true
   tags = {
-    Name = var.peering_id
-  }
-}
-
-####################################################################################################
-### VAULT
-####################################################################################################
-resource "vault_namespace" "develop" {
-  path = "develop"
-}
-
-resource "vault_mount" "apps" {
-  namespace = vault_namespace.develop.path
-  path      = "apps"
-  type      = "kv-v2"
-}
-
-resource "vault_kv_secret_v2" "boundary" {
-  namespace           = vault_namespace.develop.path
-  mount               = vault_mount.apps.path
-  name                = "infra/boundary"
-  cas                 = 1
-  delete_all_versions = true
-  data_json = jsonencode(
-    {
-      username = local.boundary_username,
-      password = local.boundary_password
-    }
-  )
-  custom_metadata {
-    max_versions = 5
-    data = {
-      tf_module = "hcp-cloud"
-    }
+    Name = local.peering_id
   }
 }
 
 ####################################################################################################
 ### VAULT - JWT AUTH ROLE FOR TFC
 ####################################################################################################
+resource "vault_namespace" "env" {
+  path = var.environment
+}
+
+resource "vault_namespace" "devops" {
+  namespace = vault_namespace.env.path
+  path      = "devops"
+}
+
 resource "vault_jwt_auth_backend" "jwt" {
-  namespace          = vault_namespace.develop.path
+  namespace          = vault_namespace.devops.path_fq
   description        = "Dynamic Credentials for Vault provider"
   path               = "jwt"
   oidc_discovery_url = "https://app.terraform.io"
   bound_issuer       = "https://app.terraform.io"
 }
 
-resource "vault_policy" "tfc_policy" {
-  for_each = var.hcp_vault_variable_set_workspaces
+resource "vault_policy" "workspaces" {
+  for_each = local.hcp_vault_variable_set_workspaces
 
   name      = each.key
-  namespace = vault_namespace.develop.path
+  namespace = vault_namespace.devops.path_fq
   policy    = file("templates/${each.key}-policy.hcl")
 }
 
-resource "vault_jwt_auth_backend_role" "tfc" {
-  for_each = var.hcp_vault_variable_set_workspaces
+resource "vault_jwt_auth_backend_role" "workspaces" {
+  for_each = local.hcp_vault_variable_set_workspaces
 
-  namespace      = vault_namespace.develop.path
+  namespace      = vault_namespace.devops.path_fq
   backend        = vault_jwt_auth_backend.jwt.path
   role_name      = each.key
-  token_policies = [each.key]
+  token_policies = ["default", each.key]
 
   bound_audiences   = ["vault.workload.identity"]
   bound_claims_type = "glob"
@@ -141,18 +117,18 @@ resource "vault_jwt_auth_backend_role" "tfc" {
 ### CREATE VARABLE SETS WITH VAULT CREDENTIALS
 ####################################################################################################
 resource "tfe_variable_set" "vault" {
-  for_each = var.hcp_vault_variable_set_workspaces
+  for_each = local.hcp_vault_variable_set_workspaces
 
-  name         = "SafaPass Sentinel - Develop - Vault (${each.key}) Credentials"
+  name         = format("SafaPass Sentinel - %s - Vault (%s) Credentials", var.environment, each.value)
   description  = "This resource is manage by Terraform"
   organization = data.hcp_organization.this.name
   workspace_ids = [
-    data.tfe_workspace_ids.workspaces.ids[each.key]
+    data.tfe_workspace_ids.workspaces.ids[each.value]
   ]
 }
 
 resource "tfe_variable" "tfc_vault_provider_auth" {
-  for_each = var.hcp_vault_variable_set_workspaces
+  for_each = local.hcp_vault_variable_set_workspaces
 
   key             = "TFC_VAULT_PROVIDER_AUTH"
   value           = "true"
@@ -161,7 +137,7 @@ resource "tfe_variable" "tfc_vault_provider_auth" {
 }
 
 resource "tfe_variable" "tfc_vault_addr" {
-  for_each = var.hcp_vault_variable_set_workspaces
+  for_each = local.hcp_vault_variable_set_workspaces
 
   key             = "VAULT_ADDR"
   value           = hcp_vault_cluster.this.vault_public_endpoint_url
@@ -169,8 +145,17 @@ resource "tfe_variable" "tfc_vault_addr" {
   variable_set_id = tfe_variable_set.vault[each.key].id
 }
 
+resource "tfe_variable" "tfc_vault_namespace" {
+  for_each = local.hcp_vault_variable_set_workspaces
+
+  key             = "VAULT_NAMESPACE"
+  value           = vault_namespace.devops.path_fq
+  category        = "env"
+  variable_set_id = tfe_variable_set.vault[each.key].id
+}
+
 resource "tfe_variable" "tfc_vault_run_role" {
-  for_each = var.hcp_vault_variable_set_workspaces
+  for_each = local.hcp_vault_variable_set_workspaces
 
   key             = "TFC_VAULT_RUN_ROLE"
   value           = each.key
