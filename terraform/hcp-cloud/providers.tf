f0165ec928154fea30fbbd30d4165e1f942ab618
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-03-10 17:45:22 +0100

    Used private enpoint in hcp_cloud module

--- terraform/hcp-cloud/providers.tf
+++ terraform/hcp-cloud/providers.tf
@@ -7,6 +7,6 @@ provider "hcp" {
 }
 
 provider "vault" {
-  address = hcp_vault_cluster.this.vault_public_endpoint_url
+  address = hcp_vault_cluster.this.vault_private_endpoint_url
   token   = hcp_vault_cluster_admin_token.this.token
 }
