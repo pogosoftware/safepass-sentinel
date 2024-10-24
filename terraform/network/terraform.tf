09ad3f3728562acbdec7913c99aac0062c6d14ab
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-10-24 13:18:34 +0200

    Upgrade network aws module version

--- terraform/network/terraform.tf
+++ terraform/network/terraform.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = "~> 1.7.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.45.0"
+      version = "5.72.1"
     }
   }
 }
